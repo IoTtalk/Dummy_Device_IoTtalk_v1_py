eceb87fa1f1912a34e34b5c2d1c7ad0c905af436
Ksoy <ksoyam95@gmail.com>
2018-04-23 18:20:05 +0800

    oo

--- DAN.py
+++ DAN.py
@@ -1,6 +1,10 @@
-import requests, time, csmapi, random, threading
+import random
+import threading
+import time
 
+from csmapi import CSMAPI
 # example
+"""
 profile = {
     'd_name': None,
     'dm_name': 'MorSensor',
@@ -8,142 +12,179 @@ profile = {
     'is_sim': False,
     'df_list': ['Acceleration', 'Temperature'],
 }
-mac_addr = 'C860008BD249'
+"""
 
-state = 'SUSPEND'     #for control channel
-#state = 'RESUME'
 
-SelectedDF = []
-control_channel_timestamp = None
-def ControlChannel():
-    global state, SelectedDF, control_channel_timestamp
-    NewSession=requests.Session()
-    while True:
-        time.sleep(2)
+class DAN():
+    def __init__(self, profile, host, mac_addr):
+        self.profile = profile
+        if host:
+            self.csmapi = CSMAPI('http://{host}:9999'.format(host=host))
+        else:
+            self.detect_local_ec()
+
+        if mac_addr:
+            self.mac_addr = mac_addr
+        else:
+            self.mac_addr = DAN.get_mac_addr()
+
+        # for control channel
+        self.state = 'SUSPEND'
+        # self.state = 'RESUME'
+
+        self.selected_DF = set()
+        self.pre_data_timestamp = {}
+        self.control_channel_timestamp = None
+        self.control_channel_thread = None
+
+    @staticmethod
+    def get_mac_addr():
+        from uuid import getnode
+        mac = getnode()
+        mac = ''.join(("%012X" % mac)[i:i + 2] for i in range(0, 12, 2))
+        return mac
+
+    def control_channel(self):
+        while True:
+            time.sleep(2)
+            try:
+                cc = self.csmapi.pull(self.mac_addr, '__Ctl_O__')
+                if not cc:
+                    continue
+
+                if self.control_channel_timestamp == cc[0][0]:
+                    continue
+
+                self.control_channel_timestamp = cc[0][0]
+                self.state = cc[0][1][0]
+                if self.state == 'SET_DF_STATUS':
+                    self.csmapi.push(self.mac_addr,
+                                     '__Ctl_I__',
+                                     ['SET_DF_STATUS_RSP',
+                                      {'cmd_params': cc[0][1][1]['cmd_params']}])
+                    df_status = list(cc[0][1][1]['cmd_params'][0])
+
+                    self.selected_DF.clear
+                    for index, status in enumerate(df_status):
+                        if status == '1':
+                            self.selected_DF.add(self.profile['df_list'][index])
+            except Exception as e:
+                print ('Control error', e)
+
+    def detect_local_ec(self):
+        import socket
+        udp_ip = ''
+        udp_port = 17000
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((udp_ip, udp_port))
+
+        while True:
+            print ('Searching for the IoTtalk server...')
+            data, addr = s.recvfrom(1024)
+            if str(data.decode()) == 'easyconnect':
+                self.csmapi.host = 'http://{}:9999'.format(addr[0])
+                break
+
+    def register_device(self):
+        """
+        profile = {
+            'd_name': None,
+            'dm_name': 'MorSensor',
+            'u_name': 'yb',
+            'is_sim': False,
+            'df_list': ['Acceleration', 'Temperature'],
+        }
+        """
+        if not self.profile.get('dm_name'):
+            raise Exception('dm_name should be given in profile.')
+
+        if not self.profile.get('d_name'):
+            self.profile['d_name'] = str(int(random.uniform(1, 100))) + '.' + self.profile['dm_name']
+
+        if not self.profile.get('u_name'):
+            self.profile['u_name'] = 'yb'
+
+        if not self.profile.get('is_sim'):
+            self.profile['is_sim'] = False
+
+        if not self.profile.get('df_list'):
+            raise Exception('df_list should be given in profile.')
+
+        print('IoTtalk Server = {}'.format(self.csmapi.host))
+        if self.csmapi.register(self.mac_addr, self.profile):
+            print ('This device has successfully registered.')
+            print ('Device name = ' + self.profile['d_name'])
+
+            if self.control_channel_thread is None:
+                print ('Create control threading')
+                # for control channel
+                self.control_channel_thread = threading.Thread(target=self.control_channel)
+                self.control_channel_thread.daemon = True
+                self.control_channel_thread.start()
+
+            return True
+        else:
+            print ('Registration failed.')
+            return False
+
+    def device_registration_with_retry(self):
+
+        while True:
+            try:
+                if self.register_device():
+                    break
+            except Exception as e:
+                print ('Attach failed: '),
+                print (e)
+            time.sleep(1)
+
+    def pull(self, df_name):
+        if self.state == 'RESUME':
+            data = self.csmapi.pull(self.mac_addr, df_name)
+            if self.pre_data_timestamp.get(df_name) != data[0][0]:
+                self.pre_data_timestamp[df_name] = data[0][0]
+
+                if data[0][1]:
+                    return data[0][1]
+
+        return None
+
+    def pull_with_timestamp(self, df_name):
+        if self.state == 'RESUME':
+            data = self.csmapi.pull(self.mac_addr, df_name)
+            if self.pre_data_timestamp.get(df_name) != data[0][0]:
+                self.pre_data_timestamp[df_name] = data[0][0]
+
+                if data[0][1]:
+                    return (data[0][0], data[0][1])
+
+        return None
+
+    def push(self, df_name, *data):
+        if self.state == 'RESUME':
+            return self.csmapi.push(self.mac_addr, df_name, list(data))
+
+        return None
+
+    def get_alias(self, df_name):
         try:
-            CH = csmapi.pull(MAC,'__Ctl_O__', NewSession)
-            if CH != []:
-                if control_channel_timestamp == CH[0][0]: continue
-                control_channel_timestamp = CH[0][0]
-                state = CH[0][1][0]
-                if state == 'SET_DF_STATUS' :
-                    csmapi.push(MAC,'__Ctl_I__',['SET_DF_STATUS_RSP',{'cmd_params':CH[0][1][1]['cmd_params']}], NewSession)
-                    DF_STATUS = list(CH[0][1][1]['cmd_params'][0])
-                    SelectedDF = []
-                    index=0            
-                    for STATUS in DF_STATUS:
-                        if STATUS == '1':
-                            SelectedDF.append(profile['df_list'][index])
-                        index=index+1
+            alias = self.csmapi.get_alias(self.mac_addr, df_name)
         except Exception as e:
-            print ('Control error', e)
-            
-
-
-def get_mac_addr():
-    from uuid import getnode
-    mac = getnode()
-    mac = ''.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
-    return mac
-
-def detect_local_ec():
-    EASYCONNECT_HOST=None
-    import socket
-    UDP_IP = ''
-    UDP_PORT = 17000
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((UDP_IP, UDP_PORT))
-    while EASYCONNECT_HOST==None:
-        print ('Searching for the IoTtalk server...')
-        data, addr = s.recvfrom(1024)
-        if str(data.decode()) == 'easyconnect':
-            EASYCONNECT_HOST = 'http://{}:9999'.format(addr[0])
-            csmapi.ENDPOINT=EASYCONNECT_HOST
-            #print('IoTtalk server = {}'.format(csmapi.ENDPOINT))
-
-timestamp={}
-MAC=get_mac_addr()
-thx=None
-def register_device(addr):
-    global MAC, profile, timestamp, thx
-
-    if csmapi.ENDPOINT == None: detect_local_ec()
-
-    if addr != None: MAC = addr
-
-    if profile['d_name'] == None: profile['d_name']= str(int(random.uniform(1, 100)))+'.'+ profile['dm_name']
-
-    for i in profile['df_list']: timestamp[i] = ''
-
-    print('IoTtalk Server = {}'.format(csmapi.ENDPOINT))
-    if csmapi.register(MAC,profile):
-        print ('This device has successfully registered.')
-        print ('Device name = ' + profile['d_name'])
-         
-        if thx == None:
-            print ('Create control threading')
-            thx=threading.Thread(target=ControlChannel)     #for control channel
-            thx.daemon = True                               #for control channel
-            thx.start()                                     #for control channel 
-        
-        return True
-    else:
-        print ('Registration failed.')
-        return False
-
-def device_registration_with_retry(IP=None, addr=None):
-    if IP != None:
-        csmapi.ENDPOINT = 'http://' + IP + ':9999'
-    success = False
-    while not success:
-        try:
-            register_device(addr)
-            success = True
-        except Exception as e:
-            print ('Attach failed: '),
-            print (e)
-        time.sleep(1)
-
-def pull(FEATURE_NAME):
-    global timestamp
-
-    if state == 'RESUME': data = csmapi.pull(MAC,FEATURE_NAME)
-    else: data = []
-        
-    if data != []:
-        if timestamp[FEATURE_NAME] == data[0][0]:
+            # print (e)
             return None
-        timestamp[FEATURE_NAME] = data[0][0]
-        if data[0][1] != []:
-            return data[0][1]
-        else: return None
-    else:
-        return None
 
-def push(FEATURE_NAME, *data):
-    if state == 'RESUME':
-        return csmapi.push(MAC, FEATURE_NAME, list(data))
-    else: return None
-
-def get_alias(FEATURE_NAME):
-    try:
-        alias = csmapi.get_alias(MAC,FEATURE_NAME)
-    except Exception as e:
-        #print (e)
-        return None
-    else:
         return alias
 
-def set_alias(FEATURE_NAME, alias):
-    try:
-        alias = csmapi.set_alias(MAC, FEATURE_NAME, alias)
-    except Exception as e:
-        #print (e)
-        return None
-    else:
+    def set_alias(self, df_name, new_alias_name):
+        try:
+            alias = self.csmapi.set_alias(self.mac_addr, df_name, new_alias_name)
+        except Exception as e:
+            # print (e)
+            return None
+
         return alias
 
-		
-def deregister():
-    return csmapi.deregister(MAC)
+    def deregister(self):
+        return self.csmapi.deregister(self.mac_addr)
--- csmapi.py
+++ csmapi.py
@@ -1,59 +1,163 @@
-import requests
+import requests, threading
 
-ENDPOINT = None
-TIMEOUT=10
-IoTtalk = requests.Session()
-#print(IoTtalk)
+from functools import wraps
+
+session_pool = {}
 
 class CSMError(Exception):
     pass
 
-def register(mac_addr, profile, UsingSession=IoTtalk):
-    r = UsingSession.post(
-        ENDPOINT + '/' + mac_addr,
-        json={'profile': profile}, timeout=TIMEOUT
-    )
-    if r.status_code != 200: raise CSMError(r.text)
-    return True
+def session_wrapper(func):
+
+    @wraps(func)
+    def wrap(interface, *args, **kwargs):
+        global session_pool
+        t_id = threading.get_ident()
+        if t_id in (session_pool):
+            session = session_pool.get(t_id)
+        else:
+            session = requests.Session()
+            session_pool[t_id] = session
+
+        if not interface.host:
+            raise CSMError('no host given')
+
+        if interface.session:
+            result = func(interface, *args, **kwargs)
+        else:
+            interface.session = session
+
+            try:
+                result = func(interface, *args, **kwargs)
+            except Exception as e:
+                if interface.session:
+                    interface.session = None
+
+                raise e
+
+            interface.session = None
+
+        return result
+
+    return wrap
 
 
-def deregister(mac_addr, UsingSession=IoTtalk):
-    r = UsingSession.delete(ENDPOINT + '/' + mac_addr)
-    if r.status_code != 200: raise CSMError(r.text)
-    return True
+class CSMAPI():
+    def __init__(self, host):
+        self.TIMEOUT = 10
+        self.host = host
+        self.session = None
 
+    @session_wrapper
+    def register(self, mac_addr, profile):
+        url = '{host}/{mac_addr}'.format(
+            host=self.host,
+            mac_addr=mac_addr
+        )
+        response = self.session.post(
+            url,
+            json={'profile': profile},
+            timeout=self.TIMEOUT
+        )
 
-def push(mac_addr, df_name, data, UsingSession=IoTtalk):
-    r = UsingSession.put(
-        ENDPOINT + '/' + mac_addr + '/' + df_name,
-        json={'data': data}, timeout=TIMEOUT
-    )
-    if r.status_code != 200: raise CSMError(r.text)
-    return True
+        response.close()
+        if response.status_code != 200:
+            raise CSMError(response.text)
 
+        return True
 
-def pull(mac_addr, df_name, UsingSession=IoTtalk):
-    #print(UsingSession)
-    r = UsingSession.get(ENDPOINT + '/' + mac_addr + '/' + df_name, timeout=TIMEOUT)
-    if r.status_code != 200: raise CSMError(r.text)
-    return r.json()['samples']
+    @session_wrapper
+    def deregister(self, mac_addr):
+        url = '{host}/{mac_addr}'.format(
+            host=self.host,
+            mac_addr=mac_addr
+        )
+        response = self.session.delete(url)
 
+        response.close()
+        if response.status_code != 200:
+            raise CSMError(response.text)
 
-def get_alias(mac_addr, df_name, UsingSession=IoTtalk):
-    r = UsingSession.get(ENDPOINT + '/get_alias/' + mac_addr + '/' + df_name, timeout=TIMEOUT)
-    if r.status_code != 200: raise CSMError(r.text)
-    return r.json()['alias_name']
+        return True
 
-	
-def set_alias(mac_addr, df_name, s, UsingSession=IoTtalk):
-    r = UsingSession.get(ENDPOINT + '/set_alias/' + mac_addr + '/' + df_name + '/alias?name=' + s, timeout=TIMEOUT)
-    if r.status_code != 200: raise CSMError(r.text)
-    return True
+    @session_wrapper
+    def push(self, mac_addr, df_name, data):
+        url = '{host}/{mac_addr}/{df_name}'.format(
+            host=self.host,
+            mac_addr=mac_addr,
+            df_name=df_name
+        )
+        response = self.session.put(
+            url,
+            json={'data': data},
+            timeout=self.TIMEOUT
+        )
 
-	
-def tree(UsingSession=IoTtalk):
-    r = UsingSession.get(ENDPOINT + '/tree')
-    if r.status_code != 200: raise CSMError(r.text)
-    return r.json()
+        response.close()
+        if response.status_code != 200:
+            raise CSMError(response.text)
 
-	
+        return True
+
+    @session_wrapper
+    def pull(self, mac_addr, df_name):
+        url = '{host}/{mac_addr}/{df_name}'.format(
+            host=self.host,
+            mac_addr=mac_addr,
+            df_name=df_name
+        )
+        response = self.session.get(
+            url,
+            timeout=self.TIMEOUT
+        )
+
+        response.close()
+        if response.status_code != 200:
+            raise CSMError(response.text)
+
+        return response.json()['samples']
+
+    @session_wrapper
+    def get_alias(self, mac_addr, df_name):
+        url = '{host}/get_alias/{mac_addr}/{df_name}'.format(
+            host=self.host,
+            mac_addr=mac_addr,
+            df_name=df_name
+        )
+        response = self.session.get(
+            url,
+            timeout=self.TIMEOUT)
+
+        response.close()
+        if response.status_code != 200:
+            raise CSMError(response.text)
+
+        return response.json()['alias_name']
+
+    @session_wrapper
+    def set_alias(self, mac_addr, df_name, new_alias):
+        url = '{host}/set_alias/{mac_addr}/{df_name}/alias?name={new_alias}'.format(
+            host=self.host,
+            mac_addr=mac_addr,
+            df_name=df_name,
+            new_alias=new_alias
+        )
+        response = self.session.get(
+            url,
+            timeout=self.TIMEOUT)
+
+        response.close()
+        if response.status_code != 200:
+            raise CSMError(response.text)
+        return True
+
+    @session_wrapper
+    def tree(self):
+        url = '{host}/tree'.format(host=self.host)
+        response = self.session.get(url)
+
+        response.close()
+        if response.status_code != 200:
+            raise CSMError(response.text)
+
+        return response.json()
