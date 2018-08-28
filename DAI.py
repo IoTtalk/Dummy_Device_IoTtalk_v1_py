c835201f0dd4f5c3ebbaed31f6e5492d1a2a5adc
Jyneda <jyneda@gmail.com>
2018-08-28 23:52:24 +0800

    Add files via upload
    Device authentication is supported.

---
+++ DAI.py
@@ -0,0 +1,34 @@
+import time, DAN, requests, random
+
+ServerURL = 'http://IP:9999' #with no secure connection
+#ServerURL = 'https://DomainName' #with SSL connection
+Reg_addr = None #if None, Reg_addr = MAC address
+
+DAN.profile['dm_name']='Dummy_Device'
+DAN.profile['df_list']=['Dummy_Sensor', 'Dummy_Control']
+DAN.profile['d_name']= None # None for autoNaming
+DAN.device_registration_with_retry(ServerURL, Reg_addr)
+
+while True:
+    try:
+    #Pull data from a device feature called "Dummy_Control"
+        value1=DAN.pull('Dummy_Control')
+        if value1 != None:
+            print (value1[0])
+
+    #Push data to a device feature called "Dummy_Sensor"
+        value2=random.uniform(1, 10)
+        DAN.push ('Dummy_Sensor', value2,  value2)
+
+
+    except Exception as e:
+        print(e)
+        if str(e).find('mac_addr not found:') != -1:
+            print('Reg_addr is not found. Try to re-register...')
+            DAN.device_registration_with_retry(ServerURL, Reg_addr)
+        else:
+            print('Connection failed due to unknow reasons.')
+            time.sleep(1)    
+
+    time.sleep(0.2)
+
---
+++ csmapi.py
@@ -0,0 +1,66 @@
+import requests
+
+ENDPOINT = None
+TIMEOUT=10
+IoTtalk = requests.Session()
+passwordKey = None
+
+class CSMError(Exception):
+    pass
+
+def register(mac_addr, profile, UsingSession=IoTtalk):
+    global passwordKey
+    r = UsingSession.post(
+        ENDPOINT + '/' + mac_addr,
+        json={'profile': profile}, timeout=TIMEOUT
+    )
+    if r.status_code != 200: raise CSMError(r.text)
+    else: passwordKey = r.json().get('password')
+    return True
+
+
+def deregister(mac_addr, UsingSession=IoTtalk):
+    r = UsingSession.delete(ENDPOINT + '/' + mac_addr)
+    if r.status_code != 200: raise CSMError(r.text)
+    return True
+
+
+def push(mac_addr, df_name, data, UsingSession=IoTtalk):
+    r = UsingSession.put(
+        ENDPOINT + '/' + mac_addr + '/' + df_name,
+        json={'data': data},
+        timeout=TIMEOUT,
+        headers = {'password-key': passwordKey}
+    )
+    if r.status_code != 200: raise CSMError(r.text)
+    return True
+
+
+def pull(mac_addr, df_name, UsingSession=IoTtalk):
+    r = UsingSession.get(
+        ENDPOINT + '/' + mac_addr + '/' + df_name,
+        timeout=TIMEOUT,
+        headers = {'password-key': passwordKey}
+    )
+    if r.status_code != 200: raise CSMError(r.text)
+    return r.json()['samples']
+
+
+def get_alias(mac_addr, df_name, UsingSession=IoTtalk):
+    r = UsingSession.get(ENDPOINT + '/get_alias/' + mac_addr + '/' + df_name, timeout=TIMEOUT)
+    if r.status_code != 200: raise CSMError(r.text)
+    return r.json()['alias_name']
+
+	
+def set_alias(mac_addr, df_name, s, UsingSession=IoTtalk):
+    r = UsingSession.get(ENDPOINT + '/set_alias/' + mac_addr + '/' + df_name + '/alias?name=' + s, timeout=TIMEOUT)
+    if r.status_code != 200: raise CSMError(r.text)
+    return True
+
+	
+def tree(UsingSession=IoTtalk):
+    r = UsingSession.get(ENDPOINT + '/tree')
+    if r.status_code != 200: raise CSMError(r.text)
+    return r.json()
+
+	
